9ed8394983f3b165f909864a9ddea4f2449ffc36
Chris Wilson <wilburwilf@protonmail.com>
2024-04-22 18:17:35 +0100

    add s3 config

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "http_api_url" {
   value = aws_apigatewayv2_api.my_http_api.api_endpoint
 }
+
 output "cognito_app_client_id" {
   value = aws_cognito_user_pool_client.my_user_pool_client.id
 }
