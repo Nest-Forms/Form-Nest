1686412ba371ea909db3e7098f0ed6fd26bd01c9
Chris Wilson <wilburwilf@protonmail.com>
2024-04-22 14:11:29 +0100

    only build if changed

--- .github/workflows/scripts/check-commit.sh
+++ .github/workflows/scripts/check-commit.sh
@@ -1,19 +1,30 @@
 #/bin/bash
 folderToIgnore=$1
 
+echo folderToIgnore $folderToIgnore
+
 echo "=============== list modified files ==============="
-git diff --name-only HEAD^ HEAD
+git diff --name-only HEAD~1 HEAD
+# git diff --name-only HEAD^ HEAD
 
 echo "========== check paths of modified files =========="
-git diff --name-only HEAD^ HEAD &gt; files.txt
+# git diff --name-only HEAD^ HEAD &gt; files.txt
+git diff --name-only HEAD~1 HEAD > files.txt
+
 while IFS= read -r file
 do
-echo $file
-if [[ $file != $folderToIgnore/* ]]; then
-    echo "This modified file is not under the '$folderToIgnore' folder."
-    echo "::set-output name=run_job::false"
-    break
-else
-    echo "::set-output name=run_job::true"
-fi
-done &lt; files.txt
+    echo $file
+    if [[ $file != $folderToIgnore/* ]]; then
+        echo "This modified file is not under the '$folderToIgnore' folder."
+        eval "$folderToIgnore=false"
+        #echo "::set-output name=run_job::false"
+        break
+    else
+        #echo "::set-output name=run_job::true"
+        eval "$folderToIgnore=true"
+    fi
+done < files.txt
+
+echo folderToIgnore $folderToIgnore
+
+export $folderToIgnore
