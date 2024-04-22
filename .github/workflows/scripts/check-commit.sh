bf281f1a7ba9e57433fbfd241be1a8e61b03c28b
Chris Wilson <wilburwilf@protonmail.com>
2024-04-22 14:13:02 +0100

    only build if changed

--- .github/workflows/scripts/check-commit.sh
+++ .github/workflows/scripts/check-commit.sh
@@ -5,28 +5,28 @@ echo folderToIgnore $folderToIgnore
 
 git log
 
-echo "=============== list modified files ==============="
-git diff --name-only HEAD~1 HEAD
-# git diff --name-only HEAD^ HEAD
+# echo "=============== list modified files ==============="
+# git diff --name-only HEAD~1 HEAD
+# # git diff --name-only HEAD^ HEAD
 
-echo "========== check paths of modified files =========="
-# git diff --name-only HEAD^ HEAD &gt; files.txt
-git diff --name-only HEAD~1 HEAD > files.txt
+# echo "========== check paths of modified files =========="
+# # git diff --name-only HEAD^ HEAD &gt; files.txt
+# git diff --name-only HEAD~1 HEAD > files.txt
 
-while IFS= read -r file
-do
-    echo $file
-    if [[ $file != $folderToIgnore/* ]]; then
-        echo "This modified file is not under the '$folderToIgnore' folder."
-        eval "$folderToIgnore=false"
-        #echo "::set-output name=run_job::false"
-        break
-    else
-        #echo "::set-output name=run_job::true"
-        eval "$folderToIgnore=true"
-    fi
-done < files.txt
+# while IFS= read -r file
+# do
+#     echo $file
+#     if [[ $file != $folderToIgnore/* ]]; then
+#         echo "This modified file is not under the '$folderToIgnore' folder."
+#         eval "$folderToIgnore=false"
+#         #echo "::set-output name=run_job::false"
+#         break
+#     else
+#         #echo "::set-output name=run_job::true"
+#         eval "$folderToIgnore=true"
+#     fi
+# done < files.txt
 
-echo folderToIgnore $folderToIgnore
+# echo folderToIgnore $folderToIgnore
 
 export $folderToIgnore
