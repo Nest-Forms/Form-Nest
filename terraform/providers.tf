96962511ad2ab7e685cbf73f794c800fab280b90
Chris Wilson <wilburwilf@protonmail.com>
2024-04-24 19:36:46 +0100

    refactored terraform files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.45.0"
+      source                = "hashicorp/aws"
+      version               = "~> 5.45.0"
       configuration_aliases = [aws.cloudfront]
     }
     archive = {
